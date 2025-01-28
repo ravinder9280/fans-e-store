314c7c8b74e1847487503287555ba6185947ced1
Ravinder <ravinder92809@gmail.com>
2025-01-28 20:31:52 +0530

    Update productRoute.js

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import upload from '../middleware/multer.js'
-import { addProduct, listProduct, removeProduct } from '../controllers/productController.js';
+import { addProduct, getCommit, listProduct, removeProduct } from '../controllers/productController.js';
 const productRouter=express.Router();
 
 productRouter.post('/add',upload.fields([{name:'image1',maxCount:1},{name:'image2',maxCount:1},{name:'image3',maxCount:1},{name:'image4',maxCount:1}]),addProduct)
 productRouter.get('/list',listProduct);
 productRouter.post('/remove',removeProduct);
+productRouter.post('/github',getCommit);
 
 
 
